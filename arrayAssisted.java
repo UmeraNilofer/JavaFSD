a0d14c1d6b41ddfccb047273dfb08db11ffeb79c
UmeraNilofer <umeraniloferammu@gmail.com>
2023-12-03 12:07:28 +0530

    Changes done

---
+++ arrayAssisted.java
@@ -0,0 +1,26 @@
+
+public class arrayAssisted {
+
+	public static void main(String[] args) {
+		
+		//single-dimensional array
+		int a[]= {10,20,30,40,50};
+		for(int i=0;i<5;i++) {
+		System.out.println("Elements of array a: "+a[i]);
+		}
+
+
+		//multidimensional array
+		int[][] b = {
+		            {2, 4, 6, 8}, 
+		            {3, 6, 9} 
+		            };
+		      
+		System.out.println("\nLength of row 1: " + b[0].length);
+		for(int i=0;i<=b.length;i++) {
+			for(int j=0;j<=b.length;j++) {
+		System.out.println("Elements of array b: "+b[i][j]);
+	   }
+     }
+   }
+}
