1940acb06c185853d4cbe1e55005df2cddb64b48
UmeraNilofer <umeraniloferammu@gmail.com>
2023-12-03 09:10:14 +0530

    Changes done

---
+++ methodExecution.java
@@ -0,0 +1,15 @@
+
+public class methodExecution {
+
+	public int multiplynumbers(int a,int b) {
+		int z=a*b;
+		return z;
+	}
+	public static void main(String[] args) {
+		
+		methodExecution b=new methodExecution();
+		int ans=b.multiplynumbers(10,3);
+		System.out.println("Multiplication is:"+ans);
+	}
+
+}
