da6fd6c723aff441e11a520d12e29eb279e7919f
UmeraNilofer <umeraniloferammu@gmail.com>
2023-12-03 11:08:48 +0530

    Changes done

---
+++ defConstructor.java
@@ -0,0 +1,20 @@
+//default constructor
+class EmpInfo{
+	int id;
+	String name;
+	
+	void display() {
+		System.out.println(id+" "+name);
+	}
+}
+public class defConstructor {
+
+	public static void main(String[] args) {
+		
+		EmpInfo emp1=new EmpInfo();
+		EmpInfo emp2=new EmpInfo();
+		emp1.display();
+		emp2.display();
+	}
+
+}
